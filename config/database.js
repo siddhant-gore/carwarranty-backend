f6d271d8e1700c296b2245d18f2a0de6fe604315
Siddhant Gore <siddhantbgore@gmail.com>
2023-10-23 10:17:16 +0530

    updated

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ mongoose.set("strictQuery", false);
 module.exports = {
   connectDatabase: () => {
     mongoose
-      .connect(process.env.DATABASE_URL)
+      .connect(process.env.DATABASE)
       .then((data) => {
         console.log(`Database server connected at port: ${data.connection.port}`);
         console.log(`Database server connected at host: ${data.connection.host}`);
